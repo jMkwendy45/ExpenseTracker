1dc51b3041127142a93482250528a65cc3232796
Duru Oluchi M <duwend46@gmail.com>
2023-11-06 13:23:07 +0100

    expenses]

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -9,15 +9,18 @@ import { GlobalProvider } from './context/GlobalState'
 
 function App() {
   return (
-      <GlobalProvider>
-          <Header />
-          <div className="container">
-              <Balance />
-              <IncomeExpenses />
-              <TransactionList />
-              <AddTransaction />
-          </div>
-      </GlobalProvider>
+      <div>
+          <GlobalProvider>
+              <Header />
+              <div className="container">
+                  <Balance />
+                  <IncomeExpenses />
+                  <TransactionList />
+                  <AddTransaction />
+              </div>
+          </GlobalProvider>
+      </div>
+
   );
 }
 
